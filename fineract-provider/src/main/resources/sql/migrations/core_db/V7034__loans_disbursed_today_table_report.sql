86c4881fbda53134200405af38b3f45d2e2fb36f
Kuziwa Sachikonye <treyviis@gmail.com>
2022-03-01 08:13:49 +0200

    closed version

--- fineract-provider/src/main/resources/sql/migrations/core_db/V7034__loans_disbursed_today_table_report.sql
+++ fineract-provider/src/main/resources/sql/migrations/core_db/V7034__loans_disbursed_today_table_report.sql
@@ -2,7 +2,7 @@
 -- Added 22/02/2022 
 -- First query thing seems to work let me look for more data 
 
-INSERT INTO `stretchy_report` VALUES (null ,'LoansDisbursedToday', 'Table', '', 'Loan', 'SELECT m.amount as amount FROM m_loan_transaction m WHERE m.transaction_date = CURDATE() AND m.transaction_type_enum = 1 AND m.office_id = ${officeId}','Used For keeping track of loans disbursed today', 0, 1);
+INSERT INTO `stretchy_report` VALUES (null ,'LoansDisbursedToday', 'Table', '', 'Loan', 'SELECT ifnull(sum(m.amount),0) as amount FROM m_loan_transaction m WHERE m.transaction_date = CURDATE() AND m.transaction_type_enum = 1 AND m.office_id = ${officeId}','Used For keeping track of loans disbursed today', 0, 1);
 
 INSERT INTO stretchy_report_parameter ( report_id, parameter_id, report_parameter_name) 
 VALUES ((select sr.id from stretchy_report sr where sr.report_name='LoansDisbursedToday'),
--- fineract-provider/src/main/resources/sql/migrations/core_db/V7036__total_in_npa_table.sql
+++ fineract-provider/src/main/resources/sql/migrations/core_db/V7036__total_in_npa_table.sql
@@ -1,5 +1,5 @@
 
-INSERT INTO `stretchy_report` VALUES (null ,'TotalInNPA', 'Table', '', 'Loan', 'SELECT SUM(ml.total_outstanding_derived) AS due FROM m_loan ml JOIN m_client mc ON mc.id = ml.client_id WHERE ml.is_npa = false AND mc.office_id = ${officeId}
+INSERT INTO `stretchy_report` VALUES (null ,'TotalInNPA', 'Table', '', 'Loan', 'SELECT ifnull(SUM(ml.total_outstanding_derived),0) AS due FROM m_loan ml JOIN m_client mc ON mc.id = ml.client_id WHERE ml.is_npa = false AND mc.office_id = ${officeId}
 ','Total Value of loans in NPA', 0, 1);
 
 INSERT INTO stretchy_report_parameter ( report_id, parameter_id, report_parameter_name) 
